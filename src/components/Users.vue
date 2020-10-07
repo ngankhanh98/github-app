1fa1bb4014c70514256a4578a495419abaa54142
ngankhanh98 <ngankhanh98@gmail.com>
2020-10-07 11:21:31 +0700

    ✨ alert  when user not found

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,10 +1,15 @@
 <template>
-  <div v-for="item in items" :key="item.id">
-    <div class="card" style="width: 18rem;">
-      <div class="card-body d-flex flex-row ">
-        <img v-bind:src="item.avatar_url" width="50" height="50"/>
-        <h5 class="card-title">{{ item.login }}</h5>
-      </div>
+  <div class="media text-muted pt-3" v-for="item in items" :key="item.id">
+    <img
+      v-bind:src="item.avatar_url"
+      width="30"
+      height="30"
+      class="bd-placeholder-img mr-2 rounded"
+    />
+    <div
+      class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray d-flex justify-content-start"
+    >
+      <div><strong class="text-primary">@{{item.login}}</strong></div>
     </div>
   </div>
 </template>
