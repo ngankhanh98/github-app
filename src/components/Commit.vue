e5f62942d5ee7eae27d19e1eb58792b07f658a48
ngankhanh98 <ngankhanh98@gmail.com>
2020-10-08 14:13:21 +0700

    ✨ click a commit, open it in new tab

--- src/components/Commit.vue
+++ src/components/Commit.vue
@@ -1,11 +1,26 @@
 <template>
-  <div></div>
+  <div class="card">
+    <div class="card-body" :key="sha">
+      <a :href="html_url" target="_blank">
+        <h6>{{ message }}</h6>
+      </a>
+      <p class="text-muted">
+        <a href="#" class="text-reset">{{ author }}</a>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Commit",
+  props: {
+    author: { type: String },
+    message: { type: String },
+    sha: { type: String },
+    html_url: { type: String },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
--- src/views/Commits.vue
+++ src/views/Commits.vue
@@ -1,14 +1,25 @@
 <template>
-  {{ $route.params.username }}
-  {{ $route.params.repos }}
-  <div>{{ commits }}</div>
+  <div>
+    <div v-for="commit in commits" :key="commit.sha">
+      <Commit
+        :key="commit.sha"
+        :message="commit.commit.message"
+        :author="commit.author.login"
+        :html_url="commit.html_url"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import Commit from "../components/Commit";
 
 export default {
   name: "Commits",
+  components: {
+    Commit,
+  },
   mounted() {
     const username = this.$route.params.username;
     const repos = this.$route.params.repos;
