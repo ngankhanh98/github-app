44771725cc6ab4c2501d47e9c83386328d225f5a
ngankhanh98 <ngankhanh98@gmail.com>
2020-10-15 17:14:29 +0700

    :bug: fixed data

--- src/components/PersonalBar.vue
+++ src/components/PersonalBar.vue
@@ -4,8 +4,8 @@
     <!-- <span class="font-bold text-2xl">{{ user.name }}</span>
     <span class="text-xl">{{ user.login }} </span> -->
     <div class="flex flex-col mt-4">
-      <span class="font-bold text-2xl text-center">Khánh</span>
-      <span class="text-xl text-center">ngankhanh98 </span>
+      <span class="font-bold text-2xl text-center">{{ user.name }}</span>
+      <span class="text-xl text-center">{{ user.login }} </span>
     </div>
     <span class="my-4 text-xl text-gray-600 w-full">
       <small>{{ user.bio }} </small></span
